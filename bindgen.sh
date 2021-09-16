0898737bc4d7c004a559325b8093f62104f8e3f7
Clemens Lutz <lutzcle@cml.li>
2021-09-16 17:31:22 +0200

    Allow non-Rusty names in the generated bindings

--- bindgen.sh
+++ bindgen.sh
@@ -6,6 +6,7 @@ bindgen \
   --whitelist-function="^likwid_marker.*" \
   --default-enum-style=rust \
   --no-doc-comments \
+  --raw-line="#![allow(non_snake_case, non_camel_case_types, non_upper_case_globals)]" \
   wrapper.h -- -I"$LIKWID_PATH/include" \
   > src/likwid.rs
 
